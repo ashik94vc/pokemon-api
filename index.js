465ff49d7cd123f8d8ce0e46245a0edb5f2ea15f
Ashik Vetrivelu <vcashik@gmail.com>
2017-03-09 10:53:46 +0530

    Added Server Index File

---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * Created by ashikvc on 9/3/17.
+ */
+var http = require('http');
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var pokemon = require('./data/pokemon.json');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+app.get('/id/:id',function(req,res){
+    var id = req.params.id;
+    if(id > pokemon.length)
+        res.send({error: 'Pokemon Not Found'});
+    res.send(pokemon[id-1]);
+});
+
